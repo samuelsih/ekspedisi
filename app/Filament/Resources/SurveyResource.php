f9be985a9f005fde8b64c41afd70d5c9554f71cc
samuelsih <samuel.205025@mhs.its.ac.id>
2025-02-17 20:04:48 +0700

    feat(filament): make survey cannot deleted again

--- app/Filament/Resources/SurveyResource.php
+++ app/Filament/Resources/SurveyResource.php
@@ -62,10 +62,6 @@ class SurveyResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make()
-                    ->after(function (Survey $record) {
-                        $record->survey_answers()->delete();
-                    }),
                 Tables\Actions\Action::make('view_survey_answers')
                     ->color('primary')
                     ->modal()
