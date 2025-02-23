c656011b7aca31584a4f536785c5b9311444608b
samuelsih <samuel.205025@mhs.its.ac.id>
2025-02-23 23:00:37 +0700

    feat: include deleted_at in query contribution widget

--- app/Filament/Widgets/TopContributionDriver.php
+++ app/Filament/Widgets/TopContributionDriver.php
@@ -24,7 +24,8 @@ class TopContributionDriver extends ChartWidget
             WITH surveys_count AS (
                 SELECT driver_id, COUNT(*) as s_count
                     FROM surveys
-                    WHERE (NULLIF(:startDate, '') IS NULL OR DATE(created_at) >= NULLIF(:startDate, ''))
+                    WHERE deleted_at IS NULL
+                    AND (NULLIF(:startDate, '') IS NULL OR DATE(created_at) >= NULLIF(:startDate, ''))
                     AND (NULLIF(:endDate, '') IS NULL OR DATE(created_at) <= NULLIF(:endDate, ''))
                     AND (NULLIF(:channelId, '') IS NULL OR channel_id = NULLIF(:channelId, ''))
                     GROUP by driver_id
@@ -32,7 +33,8 @@ class TopContributionDriver extends ChartWidget
                 customer_survey_declines_count AS (
                     SELECT driver_id, COUNT(*) AS s_count
                     FROM customer_survey_declines
-                    WHERE (NULLIF(:startDate, '') IS NULL OR DATE(created_at) >= NULLIF(:startDate, ''))
+                    WHERE deleted_at IS NULL
+                    AND (NULLIF(:startDate, '') IS NULL OR DATE(created_at) >= NULLIF(:startDate, ''))
                     AND (NULLIF(:endDate, '') IS NULL OR DATE(created_at) <= NULLIF(:endDate, ''))
                     AND (NULLIF(:channelId, '') IS NULL OR channel_id = NULLIF(:channelId, ''))
                     GROUP by driver_id
@@ -42,23 +44,25 @@ class TopContributionDriver extends ChartWidget
                     FROM survey_answers sa
                     JOIN surveys s
                     ON sa.survey_id = s.id
-                    WHERE (NULLIF(:startDate, '') IS NULL OR DATE(s.created_at) >= NULLIF(:startDate, ''))
+                    WHERE s.deleted_at IS NULL
+                    AND (NULLIF(:startDate, '') IS NULL OR DATE(s.created_at) >= NULLIF(:startDate, ''))
                     AND (NULLIF(:endDate, '') IS NULL OR DATE(s.created_at) <= NULLIF(:endDate, ''))
                     AND (NULLIF(:channelId, '') IS NULL OR s.channel_id = NULLIF(:channelId, ''))
                     GROUP BY s.driver_id
                 )
-                SELECT
-                    d.nik, d.name,
-                    CASE WHEN (sc.s_count + COALESCE(csd.s_count, 0)) = 0 THEN 0
-                    ELSE (1.0 * sc.s_count / (sc.s_count + COALESCE(csd.s_count, 0))) * sa.avg_rating
-                    END AS driver_contribution
-                FROM drivers d
-                JOIN surveys_count sc ON d.id = sc.driver_id
-                LEFT JOIN customer_survey_declines_count csd on d.id = csd.driver_id
-                LEFT JOIN survey_answers_avg_rating sa ON d.id = sa.driver_id
-                ORDER by driver_contribution DESC
-                LIMIT 10
-                ;
+            SELECT
+                d.nik, d.name,
+                CASE WHEN (sc.s_count + COALESCE(csd.s_count, 0)) = 0 THEN 0
+                ELSE (1.0 * sc.s_count / (sc.s_count + COALESCE(csd.s_count, 0))) * sa.avg_rating
+                END AS driver_contribution
+            FROM drivers d
+            JOIN surveys_count sc ON d.id = sc.driver_id
+            LEFT JOIN customer_survey_declines_count csd on d.id = csd.driver_id
+            LEFT JOIN survey_answers_avg_rating sa ON d.id = sa.driver_id
+            WHERE d.deleted_at IS NULL
+            ORDER by driver_contribution DESC
+            LIMIT 10
+            ;
             ";
 
         $result = DB::select($stmt, [
